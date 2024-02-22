7fc8d3be5dcee3693b611e5079ed4397f0797ca5
antarthureau <88631510+antarthureau@users.noreply.github.com>
2024-02-22 18:57:55 +0100

    Update soundtolight.ino

--- soundtolight.ino
+++ soundtolight.ino
@@ -1,28 +1,51 @@
+/*
+this sketch takes 3 inputs A0 A1 A2 0-5V and converts to 3 outputs D9 D10 D11 PWM 0-255/0-5V.
+It can be used as a sound to light device, asuming a DC bias is set between the audio signals and the analog inputs.
+The PWM can control 0-5V loads, MOSFETS for bigger loads (such as 12 or 24VDC led strips), etc.
+
+The analog signal are first ranged from 0~1024 to -512~+512 to match positive/negative phases of original AC signal
+the signal is then multiplied over 0 if it is under 0, effectively turning every negative spike in a positive spike
+the signal is then divided by 2 and sent out
+*/
+
 float inputSignal;
 float outputSignal;
-int delayTime = 100;
-int inA = 0;
-int inB = 1;
-int inC = 2;
-int outA = 9;
-int outB = 10;
-int outC = 11;
-
-void soundToLight(inputPin, outputPin){
-  inputSignal = analogRead(inputPin);
-  outputSignal = inputSignal/4;
-  digitalWrite(outputPin, outputSignal);
-  delay(delayTime);
-}
+const int delayTime = 10;
+const int inA = 0;
+const int inB = 1;
+const int inC = 2;
+const int outA = 9;
+const int outB = 10;
+const int outC = 11;
 
 void setup() {
   pinMode(outA, OUTPUT);
   pinMode(outB, OUTPUT);
   pinMode(outC, OUTPUT);
+  delay(100);
+}
 
+void acToPWM(int inputPin, int outputPin){
+  inputSignal = analogRead(inputPin);          //inputSignal 0~1024
+  inputSignal -= inputSignal/2;                //inputSignal (0~1024-512)/2 = -512~512
+  if (inputSignal < 0){                        //if inputSignal reaches under 0 (=every negative phase), multiply it above 0 (- * - = +)
+    inputSignal = inputSignal * -1;
+  }
+  outputSignal = inputSignal/2;                //inputSignal (0~512)/2 becomes outputSignal 0~255, range of PWM
+  digitalWrite(outputPin, outputSignal);
+  delay(delayTime);
+}
+
+void dcToPWM(int inputPin, int outputPin){
+  inputSignal = analogRead(inputPin);
+  outputSignal = inputSignal/4;                //inputSignal (0~1024)/2 becomes outputSignal 0~255, range of PWM
+  digitalWrite(outputPin, outputSignal);
+  delay(delayTime);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //use acToPWM() or dcToPWM()
+  acToPWM(inA, outA);
+  acToPWM(inB, outB);
+  acToPWM(inC, outC);
 }
